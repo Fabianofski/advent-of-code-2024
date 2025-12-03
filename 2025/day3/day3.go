03ca51815bfe2341fd7f3cad2df3ee20f7edd32b
Fabian Friedrich <friedrich.fabian@gmx.net>
2025-12-03 10:45:07 +0100

    add: day3 2025 part 2

--- 2025/day3/day3.go
+++ 2025/day3/day3.go
@@ -8,6 +8,21 @@ import (
 	"strings"
 )
 
+func getHighestJoltage(amount int, nums []string, joltage string) string {
+	if amount == 0 {
+		return joltage
+	}
+	highestNum := "0"
+	highestIdx := 0
+	for i, num := range nums[:len(nums)-amount+1] {
+		if num > highestNum {
+			highestNum = num
+			highestIdx = i
+		}
+	}
+	return getHighestJoltage(amount-1, nums[highestIdx+1:], joltage+highestNum)
+}
+
 func main() {
 	f, _ := os.Open("test_input.txt")
 	defer f.Close()
@@ -15,21 +30,15 @@ func main() {
 	scanner := bufio.NewScanner(f)
 
 	part1 := 0
+	part2 := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		nums := strings.Split(line, "")
-
-		numConcat := "0"
-		for i, num1 := range nums {
-			for _, num2 := range nums[i+1:] {
-				concat := num1 + num2
-				if concat > numConcat {
-					numConcat = concat
-				}
-			}
-		}
-		joltage, _ := strconv.Atoi(numConcat)
-		part1 += joltage
+		joltagePart1, _ := strconv.Atoi(getHighestJoltage(2, nums, ""))
+		joltagePart2, _ := strconv.Atoi(getHighestJoltage(12, nums, ""))
+		part1 += joltagePart1
+		part2 += joltagePart2
 	}
 	fmt.Printf("Part1: %d\n", part1)
+	fmt.Printf("Part2: %d\n", part2)
 }
